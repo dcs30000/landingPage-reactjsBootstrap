540a217596cef1a5dbefc66c5e0f9db1f8fe01ca
dcs3000 <dcs3000.507.2@gmail.com>
2026-01-22 11:26:43 -0300

    retirando a linha do import index.css no arquivo main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+/*import './index.css'*/
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(

0dc1d728a3c43076de014ca2b4d0086a49a3a4ab
dcs3000 <dcs3000.507.2@gmail.com>
2026-01-23 16:40:55 -0300

    configurando o header pela primeira vez

---
+++ src/components/header/header.jsx
@@ -0,0 +1,28 @@
+import { Navbar, Nav, Container } from 'react-bootstrap'
+
+function Header() {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        {/* Logo */}
+        <Navbar.Brand href="/">
+          MinhaLogo
+        </Navbar.Brand>
+
+        {/* Botão hamburguer (mobile) */}
+        <Navbar.Toggle aria-controls="menu-principal" />
+
+        {/* Menu */}
+        <Navbar.Collapse id="menu-principal">
+          <Nav className="ms-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/sobre">Sobre</Nav.Link>
+            <Nav.Link href="/contato">Contato</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default Header

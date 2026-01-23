df8d89ba58463c14f4ff3ba940c6796ea6d61cab
dcs3000 <dcs3000.507.2@gmail.com>
2026-01-23 16:42:49 -0300

    configurando o footer pela primeira vez

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,14 @@
+import { Container } from 'react-bootstrap'
+
+function Footer() {
+  return (
+    <footer className="bg-dark text-light py-4 mt-5">
+      <Container className="text-center">
+        <p className="mb-1">© 2026 - Meu Site</p>
+        <small>Todos os direitos reservados</small>
+      </Container>
+    </footer>
+  )
+}
+
+export default Footer

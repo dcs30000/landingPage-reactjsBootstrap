4552e66adfea9fe5da635f1d6c00307657d7fbcd
dcs3000 <dcs3000.507.2@gmail.com>
2026-01-26 12:07:46 -0300

    atualizamos o arquivo Main.jsx

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,14 @@
+import { Container } from 'react-bootstrap'
+import Home from '../../pages/Home'
+
+function Main() {
+  return (
+    <main>
+      <Container className="py-5">
+        <Home />
+      </Container>
+    </main>
+  )
+}
+
+export default Main

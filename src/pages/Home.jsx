a1673670dd459d8e10840b02a15cdba8afd091ee
dcs3000 <dcs3000.507.2@gmail.com>
2026-01-26 12:09:28 -0300

    atualizamos o arquivo Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <section>
+      <h1>Bem-vindo ao meu site</h1>
+      <p>
+        Esta é a página inicial da minha aplicação SPA.
+      </p>
+    </section>
+  )
+}
+
+export default Home

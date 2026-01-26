ea237bbeca67fdab56ce732a8d0a2c704c420c25
dcs3000 <dcs3000.507.2@gmail.com>
2026-01-26 12:20:32 -0300

    atualizamos o arquivo App.jsx com os componentes Header/Main/Footer

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,15 @@ import './App.css'
 
 function App() {
   return (
-    <div className="container mt-5">
-      <h1 className="text-primary">Bootstrap funcionando 🚀</h1>
-      <button className="btn btn-success mt-3">Botão Bootstrap</button>
-    </div>
+    //<div className="container mt-5">
+      //<h1 className="text-primary">Bootstrap funcionando 🚀</h1>
+      //<button className="btn btn-success mt-3">Botão Bootstrap</button>
+    //</div>
+    <>
+       <Header/>
+       <Main/>
+       <Footer/>
+    </>
   )
 }
 

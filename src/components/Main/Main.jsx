1f60573017aba72fce08f68c75205fa326ce3382
dcs3000 <dcs3000.507.2@gmail.com>
2026-01-27 09:44:41 -0300

    criamos as rotas das paginas

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,21 @@
 import { Container } from 'react-bootstrap'
+import { Routes, Route } from 'react-router-dom'
+
+
 import Home from '../../pages/Home'
+import Sobre from '../../pages/Sobre'
+import Contato from '../../pages/Contato'
+
 
 function Main() {
   return (
     <main>
       <Container className="py-5">
-        <Home />
+        <Routes>
+           <Route path='/' element={<Home />}></Route>
+           <Route path='/sobre' element={<Sobre />}></Route>
+           <Route path='/contato' element={<Contato />}></Route> 
+        </Routes>        
       </Container>
     </main>
   )

bcdc8df9d96b37b8bf504ea09de5fe497e6986cd
dcs3000 <dcs3000.507.2@gmail.com>
2026-01-27 10:43:45 -0300

    modificando o Header, agora com aicones de login, carrinho, buscar produto

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,13 +1,14 @@
-import { Navbar, Nav, Container } from 'react-bootstrap'
+import { Navbar, Nav, Container, Form, FormControl, Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import {BsCart3, BsPerson } from 'react-icons/bs'
 
 function Header() {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg"> 
+    <Navbar bg="dark" variant="dark" expand="lg" sticky='top'> 
       <Container fluid>
         {/* Logo */}
-        <Navbar.Brand as={Link} to="/">
-          MinhaLogo
+        <Navbar.Brand as={Link} to="/" className='fw-bold'>
+          Incolar
         </Navbar.Brand>
 
         {/* Botão hamburguer (mobile) */}
@@ -15,10 +16,27 @@ function Header() {
 
         {/* Menu */}
         <Navbar.Collapse id="menu-principal">
-          <Nav className="ms-auto">
+          <Nav className="me-auto">
             <Nav.Link as={Link} to="/">Home</Nav.Link>
             <Nav.Link as={Link} to="/sobre">Sobre</Nav.Link>
             <Nav.Link as={Link} to="/contato">Contato</Nav.Link>
+          </Nav>
+
+          {/*Busca*/}
+          <Form className='d-flex me-3 my-2 my-lg-0'>
+            <FormControl type='search' placeholder='Buscar produto' className='me-2' > </FormControl>
+            <Button variant='outline-light'>Buscar</Button>
+          </Form>
+
+          {/* Icones */}
+          <Nav>
+            <Nav.Link as={Link} to="/login">
+              <BsPerson size={20}></BsPerson>
+            </Nav.Link>
+
+            <Nav.Link as={Link} to="/carrinho">
+              <BsCart3 size={20}></BsCart3>
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>

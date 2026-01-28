0be6862fad7514ce10963540729492ea469ffa1a
dcs3000 <dcs3000.507.2@gmail.com>
2026-01-28 11:18:56 -0300

    criamos o arquivo Eletronicos.jsx com uma conf. simples

---
+++ src/pages/Eletronicos.jsx
@@ -0,0 +1,10 @@
+function Eletronicos(){
+    return(
+        <>
+            <h1>Eletrônicos</h1>
+            <p>Produtos para você e o seu lar</p>
+        </>
+    )
+}
+
+export default Eletronicos

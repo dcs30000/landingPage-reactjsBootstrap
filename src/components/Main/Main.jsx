e19828155c46e8f4f6e7c693d857ee25da53ab10
dcs3000 <dcs3000.507.2@gmail.com>
2026-01-28 11:32:49 -0300

    importamos a pagina Beleza e colocamos a sua rota na função Main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom'
 
 import Home from '../../pages/Home'
 import Casa from '../../pages/Casa'
+import Beleza from '../../pages/Beleza'
 import Eletronicos from '../../pages/Eletronicos'
 import Sobre from '../../pages/Sobre'
 import Contato from '../../pages/Contato'
@@ -16,6 +17,7 @@ function Main() {
         <Routes>
            <Route path='/' element={<Home />}></Route>
            <Route path='/casa' element={<Casa />}></Route>
+           <Route path='/beleza' element={<Beleza />}></Route>
            <Route path='/eletronicos' element={<Eletronicos />}></Route>
            <Route path='/sobre' element={<Sobre />}></Route>
            <Route path='/contato' element={<Contato />}></Route> 

d36f21f2f8083b302d0acc75ac54a9da77c2ebe0
dcs3000 <dcs3000.507.2@gmail.com>
2026-01-28 15:32:47 -0300

    refiz o arquivo para ter o mega-menu

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -48,6 +48,8 @@ function Header() {
   )
 }
 */}
+
+{/*}
 import { Navbar, Nav, Container, Form, FormControl, Button, NavDropdown, Row, Col } from 'react-bootstrap'
 
 import { Link } from 'react-router-dom'
@@ -58,7 +60,7 @@ function Header() {
     <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
       <Container fluid>
 
-        {/* Logo */}
+        {/* Logo AQUI TEM OUTRO ASTERISCO/}
         <Navbar.Brand as={Link} to="/" className="fw-bold">
           Incolar
         </Navbar.Brand>
@@ -67,7 +69,7 @@ function Header() {
 
         <Navbar.Collapse id="menu-principal">
 
-          {/* MENU DE CATEGORIAS */}
+          {/* MENU DE CATEGORIAS AQUI TEM OUTRO ASTERISCO/}
           <Nav className="me-4">
 
             <NavDropdown
@@ -75,7 +77,7 @@ function Header() {
               id="categorias-dropdown"
               menuVariant="dark"
             >
-              {/* Estrutura já pronta para mega-menu */}
+              {/* Estrutura já pronta para mega-menu AQUI TEM OUTRO ASTERISCO/}
               <Container className="px-3">
                 <Row>
 
@@ -118,7 +120,7 @@ function Header() {
 
           </Nav>
 
-          {/* BUSCA */}
+          {/* BUSCA AQUI TEM OUTRO ASTERISCO/}
           <Form className="d-flex me-auto my-2 my-lg-0">
             <FormControl
               type="search"
@@ -128,7 +130,7 @@ function Header() {
             <Button variant="outline-light">Buscar</Button>
           </Form>
 
-          {/* ÍCONES */}
+          {/* ÍCONES AQUI TEM OUTRO ASTERISCO/}
           <Nav className="ms-3 align-items-center gap-2">
             <Nav.Link as={Link} to="/login">
               <BsPerson size={20} />
@@ -147,6 +149,92 @@ function Header() {
 
 export default Header
 
+*/}
 
+import { Navbar, Nav, Container, Form, FormControl, Button, NavDropdown, Row, Col } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import { BsCart3, BsPerson, BsList } from 'react-icons/bs'
 
+function Header() {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
+      <Container fluid>
+        {/* Logo */}
+        <Navbar.Brand as={Link} to="/" className="fw-bold">
+          Incolar
+        </Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="menu-principal" />
+
+        <Navbar.Collapse id="menu-principal">
+
+          {/* MENU ESQUERDA */}
+          <Nav className="me-auto">
+
+            {/* BOTÃO CATEGORIAS */}
+            <NavDropdown
+              title={
+                <span className="d-flex align-items-center gap-2">
+                  <BsList size={22} /> Categorias
+                </span>
+              }
+              id="mega-menu"
+              className="mega-menu"
+            >
+              <Container>
+                <Row className="py-3">
+
+                  <Col md={4}>
+                    <h6 className="fw-bold">Beleza</h6>
+                    <NavDropdown.Item as={Link} to="/eletronicos">Ver tudo</NavDropdown.Item>
+                    <NavDropdown.Item>Perfumes</NavDropdown.Item>
+                    <NavDropdown.Item>Skincare</NavDropdown.Item>
+                    <NavDropdown.Item>Cabelos</NavDropdown.Item>
+                  </Col>
+
+                  <Col md={4}>
+                    <h6 className="fw-bold">Casa</h6>
+                    <NavDropdown.Item as={Link} to="/casa">Ver tudo</NavDropdown.Item>
+                    <NavDropdown.Item>Móveis</NavDropdown.Item>
+                    <NavDropdown.Item>Decoração</NavDropdown.Item>
+                    <NavDropdown.Item>Cozinha</NavDropdown.Item>
+                  </Col>
+
+                  <Col md={4}>
+                    <h6 className="fw-bold">Eletrônicos</h6>
+                    <NavDropdown.Item as={Link} to="/beleza">Ver tudo</NavDropdown.Item>
+                    <NavDropdown.Item>Celulares</NavDropdown.Item>
+                    <NavDropdown.Item>TVs</NavDropdown.Item>
+                    <NavDropdown.Item>Games</NavDropdown.Item>
+                  </Col>
+
+                </Row>
+              </Container>
+            </NavDropdown>
+
+          </Nav>
+
+          {/* BUSCA */}
+          <Form className="d-flex me-3">
+            <FormControl type="search" placeholder="Buscar produto" className="me-2" />
+            <Button variant="outline-light">Buscar</Button>
+          </Form>
+
+          {/* ÍCONES */}
+          <Nav>
+            <Nav.Link as={Link} to="/login">
+              <BsPerson size={20} />
+            </Nav.Link>
+            <Nav.Link as={Link} to="/carrinho">
+              <BsCart3 size={20} />
+            </Nav.Link>
+          </Nav>
+
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default Header
 

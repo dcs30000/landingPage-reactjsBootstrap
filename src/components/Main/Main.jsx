cc754db3e139297878a6eed57c6019f4f4a2785e
dcs3000 <dcs3000.507.2@gmail.com>
2026-02-03 11:00:53 -0300

    adicionamos a importação da pagina de ofertas e a sua rota

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -6,7 +6,7 @@ import Home from '../../pages/Home'
 import Casa from '../../pages/Casa'
 import Beleza from '../../pages/Beleza'
 import Eletronicos from '../../pages/Eletronicos'
-import Ofertas from '../../pages/ofertas'
+import Ofertas from '../../pages/Ofertas'
 import Sobre from '../../pages/Sobre'
 import Contato from '../../pages/Contato'
 

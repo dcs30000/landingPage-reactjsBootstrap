13630c591f01d36fcb215a57c3bbce3604d24960
dcs3000 <dcs3000.507.2@gmail.com>
2026-02-24 14:23:38 -0300

    retirei o H1 e p do slide 1 do carousel

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -14,8 +14,8 @@ function Home() {
             <Carousel.Item>
               <div className="hero-slide hero-slide-1">
                 <Container className='hero-content'> {/* para centralizar melhor */}
-                  <h1>Bem-vindo à Incolar</h1>
-                  <p>Os melhores produtos para sua casa e tecnologia</p>
+                  {/*<h1>Bem-vindo à Incolar</h1>*/}
+                  {/*<p>Os melhores produtos para sua casa e tecnologia</p>*/}
                   <Button as={Link} to="/ofertas" variant="dark">
                     Ver Ofertas
                   </Button>

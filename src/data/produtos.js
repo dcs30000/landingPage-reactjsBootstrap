c76f71a0422c1ead7402d412dc6e6bf6b632e09b
dcs3000 <dcs3000.507.2@gmail.com>
2026-03-18 12:14:53 -0300

    coloquei o nome correto das imagens

--- src/data/produtos.js
+++ src/data/produtos.js
@@ -3,21 +3,21 @@ export const produtos = [
     id: 1,
     nome: "Refrigerante Pepsi 12 Unidades",
     preco: 34.33,
-    imagem: "/pepsi.png",
+    imagem: "/caixaMaga.png",
     rating: 4.6
   },
   {
     id: 2,
     nome: "Panela Antiaderente",
     preco: 54.99,
-    imagem: "/panela.png",
+    imagem: "/panelaMaga.png",
     rating: 4.5
   },
   {
     id: 3,
-    nome: "Kit Refrigerante Lata",
+    nome: "Windows",
     preco: 78.99,
-    imagem: "/lata.png",
+    imagem: "/winMaga.png",
     rating: 4.6
   }
 ]

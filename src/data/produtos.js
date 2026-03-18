36c887a00155d9acaed051e89d66124ec2399e07
dcs3000 <dcs3000.507.2@gmail.com>
2026-03-18 12:05:18 -0300

    coloquei export para poder usar na importacao em Home.jsx

---
+++ src/data/produtos.js
@@ -0,0 +1,23 @@
+export const produtos = [
+  {
+    id: 1,
+    nome: "Refrigerante Pepsi 12 Unidades",
+    preco: 34.33,
+    imagem: "/pepsi.png",
+    rating: 4.6
+  },
+  {
+    id: 2,
+    nome: "Panela Antiaderente",
+    preco: 54.99,
+    imagem: "/panela.png",
+    rating: 4.5
+  },
+  {
+    id: 3,
+    nome: "Kit Refrigerante Lata",
+    preco: 78.99,
+    imagem: "/lata.png",
+    rating: 4.6
+  }
+]

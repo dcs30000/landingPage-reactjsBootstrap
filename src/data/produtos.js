13abfa9df09e45c2903c24dd6c5f8a7f62c640b9
dcs3000 <dcs3000.507.2@gmail.com>
2026-03-19 10:57:05 -0300

    coloquei mais um elemnto no array p/ a section home-vitrine

--- src/data/produtos.js
+++ src/data/produtos.js
@@ -22,16 +22,22 @@ export const produtos = [
   },
   {
     id:4,
-    nome: "lixeira",
+    nome: "Lixeira",
     preco: 55.90,
     imagem: "/lixeiraMaga.png",
     rating: 4.5
   },
   {
     id:5,
-    nome: "barbeador",
+    nome: "Barbeador",
     preco: 34.75,
     imagem: "/barbeadorMaga.png",
     rating: 4.6
+  },
+  {
+    id:6,
+    nome: "Bebida energética Red Bull",
+    preco: 24.95,
+    imagem: "/public/redbullMaga.png" 
   }
 ]

4654fb9dbf67d72cb794f002e036f65d5ada838b
dcs3000 <dcs3000.507.2@gmail.com>
2026-03-19 10:44:51 -0300

    coloquei alguns produtos no array para a section home-vitrine

--- src/data/produtos.js
+++ src/data/produtos.js
@@ -19,5 +19,19 @@ export const produtos = [
     preco: 78.99,
     imagem: "/winMaga.png",
     rating: 4.6
+  },
+  {
+    id:4,
+    nome: "lixeira",
+    preco: 55.90,
+    imagem: "/lixeiraMaga.png",
+    rating: 4.5
+  },
+  {
+    id:5,
+    nome: "barbeador",
+    preco: 34.75,
+    imagem: "/barbeadorMaga.png",
+    rating: 4.6
   }
 ]
